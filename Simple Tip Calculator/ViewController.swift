6ff204fe8696e5ea5096ddcff3ccead8da399708
Martin Lok <martinlok@Martins-MacBook-Pro-4.local>
2014-10-08 22:58:05 +0200

    Initial Commit

---
+++ Simple Tip Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Tip Calculator
+//
+//  Created by Martin Lok on 08/10/14.
+//  Copyright (c) 2014 Martin Lok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Simple Tip CalculatorTests/Simple_Tip_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_Tip_CalculatorTests.swift
+//  Simple Tip CalculatorTests
+//
+//  Created by Martin Lok on 08/10/14.
+//  Copyright (c) 2014 Martin Lok. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Simple_Tip_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
